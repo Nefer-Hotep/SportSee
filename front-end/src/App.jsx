b421d73116131bbd133c8be1146c0b66f97ffc02
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2024-01-08 11:27:32 +0100

    feat : add react-router-dom, sass, Header and Aside components - (dependencies) react-router-dom is added to handle the route of the app - (dependencies) sass will be used for the styling - add the first two components Header and Aside to the app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Header from './components/Header';
+import Aside from './components/Aside';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <Aside />
+      </>
+    ),
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ front-end/src/components/Aside.jsx
@@ -0,0 +1,13 @@
+function Aside() {
+
+    
+  return (
+    <>
+      <aside>
+        <span>Copyright, SportSee 2020</span>
+      </aside>
+    </>
+  );
+}
+
+export default Aside;
